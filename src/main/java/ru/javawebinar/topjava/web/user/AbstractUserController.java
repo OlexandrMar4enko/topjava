f9caf0971e51d8b476a3757796ffe86e442cac93
aleks802 <aleks.marchenko802@gmail.com>
2023-07-08 00:19:36 +0300

    2_7_annotation_processing

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -17,17 +17,18 @@ public abstract class AbstractUserController {
     @Autowired
     private UserService service;
 
-    public List<User> getAll() {
+        public List<User> getAll() {
         log.info("getAll");
         return service.getAll();
     }
 
     public User get(int id) {
         log.info("get {}", id);
-        return service.get(id);
+            return service.get(id);
     }
 
-    public User create(User user) {
+    public User create(User user)
+    {
         log.info("create {}", user);
         checkNew(user);
         return service.create(user);
